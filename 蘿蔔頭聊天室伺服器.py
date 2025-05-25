acb423216e2603b3acda59374fd052dec735b2b9
Ro2013 <938689@st.tc.edu.tw>
2025-05-25 15:42:55 +0800

    Update 蘿蔔頭聊天室伺服器.py

--- 蘿蔔頭聊天室伺服器.py
+++ 蘿蔔頭聊天室伺服器.py
@@ -1,71 +1,75 @@
-
+import asyncio
 import websockets
 import json
 import os
 
 HOST = '0.0.0.0'
 PORT = 12345
-
 USER_FILE = 'users.json'
+
+# 初始化使用者資料檔案
 if not os.path.exists(USER_FILE):
-with open(USER_FILE, 'w') as f:
-json.dump({}, f)
+    with open(USER_FILE, 'w') as f:
+        json.dump({}, f)
 
 with open(USER_FILE, 'r') as f:
-users = json.load(f)
+    users = json.load(f)
 
-clients = {} # 暱稱: websocket
+clients = {}  # 暱稱: websocket
 
 def save_users():
-with open(USER_FILE, 'w') as f:
-json.dump(users, f)
+    with open(USER_FILE, 'w') as f:
+        json.dump(users, f)
 
 async def handle_client(websocket, path):
-nickname = None
-try:
-async for message in websocket:
-request = json.loads(message)
+    nickname = None
+    try:
+        async for message in websocket:
+            request = json.loads(message)
 
-        if request['action'] == 'register':
-            nick = request['nickname']
-            pw = request['password']
-            if nick in users:
-                await websocket.send(json.dumps({'status': 'error', 'msg': '暱稱已存在'}))
-            elif any(u['password'] == pw for u in users.values()):
-                await websocket.send(json.dumps({'status': 'error', 'msg': '密碼已使用'}))
-            else:
-                users[nick] = {'password': pw}
-                save_users()
-                await websocket.send(json.dumps({'status': 'ok'}))
+            if request['action'] == 'register':
+                nick = request['nickname']
+                pw = request['password']
+                if nick in users:
+                    await websocket.send(json.dumps({'status': 'error', 'msg': '暱稱已存在'}))
+                elif any(u['password'] == pw for u in users.values()):
+                    await websocket.send(json.dumps({'status': 'error', 'msg': '密碼已使用'}))
+                else:
+                    users[nick] = {'password': pw}
+                    save_users()
+                    await websocket.send(json.dumps({'status': 'ok'}))
 
-        elif request['action'] == 'login':
-            nick = request['nickname']
-            pw = request['password']
-            if users.get(nick, {}).get('password') == pw:
-                nickname = nick
-                clients[nick] = websocket
-                await websocket.send(json.dumps({'status': 'ok'}))
-            else:
-                await websocket.send(json.dumps({'status': 'error', 'msg': '登入失敗'}))
+            elif request['action'] == 'login':
+                nick = request['nickname']
+                pw = request['password']
+                if users.get(nick, {}).get('password') == pw:
+                    nickname = nick
+                    clients[nick] = websocket
+                    await websocket.send(json.dumps({'status': 'ok'}))
+                else:
+                    await websocket.send(json.dumps({'status': 'error', 'msg': '登入失敗'}))
 
-        elif request['action'] == 'message':
-            target = request['to']
-            msg = request['msg']
-            if target in clients:
-                await clients[target].send(json.dumps({
-                    'from': nickname,
-                    'msg': msg
+            elif request['action'] == 'message':
+                target = request['to']
+                msg = request['msg']
+                if target in clients:
+                    await clients[target].send(json.dumps({
+                        'from': nickname,
+                        'msg': msg
+                    }))
+
+            elif request['action'] == 'list':
+                await websocket.send(json.dumps({
+                    'users': list(users.keys())
                 }))
 
-        elif request['action'] == 'list':
-            await websocket.send(json.dumps({
-                'users': list(users.keys())
-            }))
-finally:
-    if nickname and nickname in clients:
-        del clients[nickname]
-啟動 WebSocket 伺服器
+    except websockets.exceptions.ConnectionClosed:
+        pass
+    finally:
+        if nickname and nickname in clients:
+            del clients[nickname]
 
+# 啟動 WebSocket 伺服器
 start_server = websockets.serve(handle_client, HOST, PORT)
 print(f"WebSocket 伺服器已啟動在 ws://{HOST}:{PORT}")
 
